b29e7e61e216c922bd043731e69e134080548b31
Pavel <pavel@Pavels-MacBook-Pro.local>
2017-10-26 16:20:54 +0300

    update

--- src/main/java/com/teaminternational/web/BaseHttpServlet.java
+++ src/main/java/com/teaminternational/web/BaseHttpServlet.java
@@ -4,16 +4,13 @@ import com.teaminternational.HelloWorldGenerator;
 import com.teaminternational.HelloWorldGeneratorImpl;
 
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-import static com.teaminternational.HelloWorldGenerator.COMPONENT_NAME;
 
-@WebServlet("/")
-public abstract class BaseHttpServlet extends HttpServlet {
+public class BaseHttpServlet extends HttpServlet {
 
     protected <T> T getBean(String name) {
         return (T) getServletContext().getAttribute(name);
@@ -21,10 +18,12 @@ public abstract class BaseHttpServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doGet(req,resp);
 
-        HelloWorldGeneratorImpl helloWorldGenerator = getBean(COMPONENT_NAME);
+        HelloWorldGeneratorImpl helloWorldGenerator = getBean(HelloWorldGenerator.COMPONENT_NAME);
         helloWorldGenerator.sayHello();
 
+        System.out.println(helloWorldGenerator.sayHello());
     }
 
 }
